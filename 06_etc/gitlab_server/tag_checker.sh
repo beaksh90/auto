697f3d557929e43b730eca19ae3f327295a7683f
bsh <beaksh90@ex-em.com>
2017-02-14 11:25:46 +0900

    REMOVE OJDC5.jar from ANT SCRIPT(MFODG) import jar part tag_check will track mfobuild repo

--- 06_etc/gitlab_server/tag_checker.sh
+++ 06_etc/gitlab_server/tag_checker.sh
@@ -6,16 +6,18 @@ TAG_DIR[2]=/var/opt/gitlab/git-data/repositories/mfo/mfoweb.git; ## MFOWEB_DIR
 TAG_DIR[3]=/var/opt/gitlab/git-data/repositories/mfo/mfosql.git; ## MFOSQL_DIR
 TAG_DIR[4]=/var/opt/gitlab/git-data/repositories/mfo/mfodg.git;  ## MFODG_DIR
 TAG_DIR[5]=/var/opt/gitlab/git-data/repositories/mfo/mforts.git; ## MFORTS_DIR
+TAG_DIR[6]=/var/opt/gitlab/git-data/repositories/mfo/mfobuild.git; ## MFORTS_DIR
 WORKING_DIR=`pwd`
 sh /root/.bash_profile
 
 TAG_COUNT()
 {
-TAG_COUNT1[1]=9
-TAG_COUNT1[2]=7
-TAG_COUNT1[3]=7
-TAG_COUNT1[4]=14
-TAG_COUNT1[5]=12
+TAG_COUNT1[1]=12
+TAG_COUNT1[2]=10
+TAG_COUNT1[3]=9
+TAG_COUNT1[4]=16
+TAG_COUNT1[5]=13
+TAG_COUNT1[6]=24
 
 
 cd ${TAG_DIR[1]} ; ## MFONP_DIR
@@ -28,6 +30,10 @@ cd ${TAG_DIR[4]} ; ## MFODG_DIR
 TAG_COUNT2[4]=`git tag -l | grep -w mfodg*\_[0-9]*\.[0-9] | wc -l`
 cd ${TAG_DIR[5]} ; ## MFORTS_DIR
 TAG_COUNT2[5]=`git tag -l | grep -w mforts*\_[0-9]*\.[0-9] | wc -l`
+cd ${TAG_DIR[6]} ; ## MFORTS_DIR
+TAG_COUNT2[6]=`git tag -l | grep -w mfobuild*\_[0-9]*\.[0-9] | wc -l`
+
+MFO_BUILD_TAG_NEWEST=`git tag -l | tail -n 1`
 }
 
 TAG_DIFF_GET()
@@ -37,9 +43,10 @@ TAG_KEY[2]="mfoweb"
 TAG_KEY[3]="mfosql"
 TAG_KEY[4]="mfodg"
 TAG_KEY[5]="mforts"
+TAG_KEY[6]="mfobuild"
 UPDATER=0
 
-for i in 1 2 3 4 5
+for i in 1 2 3 4 5 6
 do
         if [ ${TAG_COUNT2[$i]} != ${TAG_COUNT1[$i]} ]; then
                         STEP=0
@@ -51,7 +58,7 @@ do
                 do
                         STEP=`expr $STEP + 1`
                         if [ ${PASS_COUNT} -lt ${STEP} ]; then
-                        echo "insert into  mfo_tag_part values('${TAG}',null);" >> insert_tag.sql
+                        echo "insert into  mfo_tag_part values('${TAG}','${MFO_BUILD_TAG_NEWEST}');" >> insert_tag.sql
                         fi
                 done
                 echo exit | sqlplus -silent git/git@DEVQA23 @insert_tag.sql
@@ -70,13 +77,14 @@ UPDATE_THIS_FILE ()
 {
 cd $WORKING_DIR
 i=0
-for i in 5 4 3 2 1
+for i in 6 5 4 3 2 1
 do
 	ATTACHMENT="TAG_COUNT1[$i]=${TAG_COUNT2[$i]}\n$ATTACHMENT";
+	echo "TAG_COUNT1[$i]=${TAG_COUNT2[$i]}"
 done
-	sed -e "13 a$ATTACHMENT" $0 > .mxctl2.sh;
+	sed -e "14 a$ATTACHMENT" $0 > .mxctl2.sh;
 
-sed -n '1,18p;25,$p' .mxctl2.sh > .mxctl.sh;
+sed -n '1,20p;28,$p' .mxctl2.sh > .mxctl.sh;
 echo  "TAG COUNT VALUE UPDATE START ======================= ";
 echo  -e "sleep 1\nmv .mxctl.sh $0\nrm -rf mxctlsaver.sh\nrm -rf .mxctl2.sh" >> mxctlsaver.sh
 echo  "chmod 775 $0" >> mxctlsaver.sh
