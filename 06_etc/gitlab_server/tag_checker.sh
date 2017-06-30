30cc923675bc345e26ea5e48258d625fcdd39fa2
bsh <beaksh90@ex-em.com>
2017-06-30 11:09:39 +0900

    tag_checker 사용하는 않는 부분 전부 삭제 및 백업

--- 06_etc/gitlab_server/tag_checker.sh
+++ 06_etc/gitlab_server/tag_checker.sh
@@ -49,8 +49,7 @@ do
                 echo "insert into mfo_tag_part values('${TAG}','${MFO_BUILD_TAG_NEWEST}');" >> insert_tag.sql
                 UPDATER=`expr $UPDATER + 1`
                 STATIC_ANALYSIS_SONARQUBE $CONF_ITEM $TAG $CONF_ITEM_GROUP $MAIN_DIR
-            ##  SEND_MAIL $CONF_ITEM $PREVIOUS_TAG $TAG $CONF_ITEM_GROUP
-        JANDI_API_CALL
+                JANDI_API_CALL
             fi
                 PREVIOUS_TAG=$TAG
         done
@@ -94,18 +93,14 @@ do
 done
 
 COMMIT_MENT_INSERT_QUERY
-if [ $NEXT_REDMINE_ISSUE_NO ]; then
-    REDMINE_API_CALL $REDMINE_ISSUE_NO
-fi
+
 unset IFS
 unset HASH_CODE
-unset REDMINE_ISSUE_NO
 }
 
 COLLECT_STATEMENT ()
 {
     DEV_MENTION="$DEV_MENTION $STATEMENT\n"
-    #TREAT_REDMINE_STATEMENT
     TREAT_JANDI_STATEMENT
     TREAT_QUERY_STATEMENT
     HASH_CODE=$NEXT_HASH_CODE
@@ -120,7 +115,6 @@ TREAT_JANDI_STATEMENT ()
             PRE_JANDI_STATEMENT=`echo $PRE_JANDI_STATEMENT | sed -re 's:\x5C\x29:\x27\x5C\x5C\x29\x27:g'` # ESCAPE   (  -> '//('
             PRE_JANDI_STATEMENT=`echo $PRE_JANDI_STATEMENT | sed -re 's:\x5C\x09:\x20:g'`                 # REMOVE   \t -> (space)
             PRE_JANDI_STATEMENT=`echo $PRE_JANDI_STATEMENT | sed -re 's:\x5C\x22::g'`                     # REMOVE   "
-            MAKE_REDMINE_HYPERLINK
 
             if [ "${NEXT_HASH_CODE}" != "${HASH_CODE}" ]&&[ ${HASH_CODE} ];
             then
@@ -128,25 +122,6 @@ TREAT_JANDI_STATEMENT ()
             fi
             JANDI_DEV_MENTION="$JANDI_DEV_MENTION $PRE_JANDI_STATEMENT \n"
     fi
-}
-
-MAKE_REDMINE_HYPERLINK()
-{
-if [ `echo $PRE_JANDI_STATEMENT | grep -E "#[0-9]{3,6}"` ];
-then
-        unset IFS
-        for FIND_NUM in $PRE_JANDI_STATEMENT
-        do
-                if [ `echo $FIND_NUM | grep -E "#[0-9]{3,5}"` ]; then
-                REDMINE_NUM=`echo $FIND_NUM | grep -E "#[0-9]{3,5}" | tr -d "#"`
-                fi
-        done
-        PRE_JANDI_STATEMENT=`echo $PRE_JANDI_STATEMENT | sed -re 's@'#$REDMINE_NUM'@['#$REDMINE_NUM'](http://115.178.73.20:32798/issues/'$REDMINE_NUM')@g'`
-        PRE_JANDI_STATEMENT="     $PRE_JANDI_STATEMENT"
-        echo $PRE_JANDI_STATEMENT
-IFS="
-"
-fi
 }
 
 # origin : https://wh.jandi.com/connect-api/webhook/11671944/f8ae192012daed50247b8c2a221f62f3
@@ -189,66 +164,12 @@ TREAT_QUERY_STATEMENT ()
     QUERY_DEV_MENTION="$QUERY_DEV_MENTION $STATEMENT'||CHR(10)||'"
 }
 
-TREAT_REDMINE_STATEMENT ()
-{
-    if [ `echo $STATEMENT | grep -E  "#[0-9]{4,5}"` ]; then
-        NEXT_REDMINE_ISSUE_NO=`echo ${STATEMENT} | grep -o "#[0-9]*" | awk -F"[#]" '{print $2}'`
-    fi
-
-    if [ $NEXT_REDMINE_ISSUE_NO ]; then
-        if [ "${NEXT_REDMINE_ISSUE_NO}" != "${REDMINE_ISSUE_NO}" ]&&[ $REDMINE_ISSUE_NO ]; then
-            REDMINE_API_CALL
-        elif [ "${NEXT_HASH_CODE}" != "${HASH_CODE}" ]&&[ $REDMINE_ISSUE_NO ] ; then
-            REDMINE_API_CALL
-            unset NEXT_REDMINE_ISSUE_NO
-            unset REDMINE_ISSUE_NO
-        fi
-        REDMINE_DEV_MENTION="$REDMINE_DEV_MENTION 
-        $STATEMENT"
-        REDMINE_ISSUE_NO=$NEXT_REDMINE_ISSUE_NO
-    fi
-}
-
 COMMIT_MENT_INSERT_QUERY ()
 {
     echo "insert into mfo_git_comment values('${TAG}','${HASH_CODE}','${QUERY_DEV_MENTION}');"
     echo "insert into mfo_git_comment values('${TAG}','${HASH_CODE}','${QUERY_DEV_MENTION}');"  >> insert_tag.sql
     unset QUERY_DEV_MENTION
 }
-
-# redmine 주소 변경
-GET_ISSUE(){
-curl --include 'http://10.10.100.230:3000/issues/${REDMINE_ISSUE_NO}.xml' 1>temp.txt 2>&1
-}
-
-# redmine 주소 변경
-UPDATE_ISSUE(){
-curl --include \
-         --request PUT \
-         --header "Content-Type: application/xml" \
-         --data-binary "<issue>
-        <subject>${SUBJECT}</subject>
-        <notes>${REDMINE_DEV_MENTION}</notes>
-        </issue>" \
-    'http://10.10.100.230:3000/issues/${REDMINE_ISSUE_NO}.xml?include=journals&key=f3bac7fb2f8e8dc26e702d15295c0d8c18ab0560' 1>/dev/null 2>$1
-}
-
-# origin : https://wh.jandi.com/connect-api/webhook/11671944/f8ae192012daed50247b8c2a221f62f3
-REDMINE_API_CALL()
-{
-        echo -e "api call : $REDMINE_DEV_MENTION"
-        echo ${REDMINE_ISSUE_NO}
-        GET_ISSUE
-
-        TEMP_TXT=`cat ./temp.txt`
-        SUBJECT=`echo $TEMP |grep -oE "(<subject>+[a-zA-Z0-9가-힣 .,:]*)<\/subject>" | awk -F"[>]" '{print $2}' | awk -F"[<]" '{print $1}'`
-        echo ${SUBJECT}
-        rm -rf ./sub.txt
-
-        UPDATE_ISSUE
-        unset REDMINE_DEV_MENTION
-}
-
 
 STATIC_ANALYSIS_SONARQUBE ()
 {
@@ -294,97 +215,6 @@ SONAR_LINK="(http://10.10.32.101:9000/dashboard/index/${CONF_ITEM_GROUP}:${CONF_
 
 SONAR_URL=" ,{\"title\":\"Sonar Qube ( metrics on new code ) :\",\"description\":\"     New Bugs :\t\t\t\t[${BUGS}]${SONAR_LINK}\n
      New Vulnerabilities :\t\t[${VUL}]${SONAR_LINK}\n     Technical Dept Ratio :\t[${DEPT}]${SONAR_LINK} % \"} "
-}
-
-SEND_MAIL ()
-{
-    WEB_FRONT_END_TEAM="hwankb@ex-em.com daru87@ex-em.com kangjm103@ex-em.com wnsrl56@ex-em.com gayoon.huh@ex-em.com"	
-
-    mfo_ALL_RECEIVER="beaksh90@naver.com mookiang@ex-em.com bsa@ex-em.com cryingpcs@ex-em.com kibbeum6738@ex-em.com hyewon@ex-em.com"
-    mfosql_RECEIVER=${WEB_FRONT_END_TEAM}
-    mfoweb_RECEIVER=${WEB_FRONT_END_TEAM}
-    mfodg_RECEIVER="ezra@ex-em.com magyeon@ex-em.com"
-    mforts_RECEIVER="jeungwoo.we@ex-em.com jcwon@ex-em.com wonsik@ex-em.com"
-    mfonp_RECEIVER="uizu99@ex-em.com hwankb@ex-em.com jcwon@ex-em.com"
-    
-    mfa_ALL_RECEIVER="kibbeum6738@ex-em.com hyewon@ex-em.com bsa@ex-em.com cryingpcs@ex-em.com"	
-    mfasql_RECEIVER=${WEB_FRONT_END_TEAM}
-    mfaweb_RECEIVER=${WEB_FRONT_END_TEAM}
-    
-    mft_ALL_RECEIVER="kibbeum6738@ex-em.com hyewon@ex-em.com bsa@ex-em.com cryingpcs@ex-em.com"
-    mftsql_RECEIVER=${WEB_FRONT_END_TEAM}
-    mftweb_RECEIVER=${WEB_FRONT_END_TEAM}
-
-    GET_WEBPAGE_SCREENSHOT $CONF_ITEM $CONF_ITEM_GROUP $PREVIOUS_TAG $TAG
-    
-    GROUP_CONF_ITEM_RECEIVER=`echo ${CONF_ITEM_GROUP}_ALL_RECEIVER` 
-    EACH_CONF_ITEM_RECEIVER=`echo ${CONF_ITEM}_RECEIVER`
-    
-    MAIL_TEXT ${!GROUP_CONF_ITEM_RECEIVER} ${!EACH_CONF_ITEM_RECEIVER}
-    
-    rm -rf ./difference*.png
-    rm -rf ./getpage.js
-}
-
-MAIL_TEXT ()
-{
-    echo -e " 1.GITLAB URL : http://10.10.32.101/${CONF_ITEM_GROUP}/${CONF_ITEM}/compare/${PREVIOUS_TAG}...${TAG}?view=parallel\n\n 2.DEV_COMMENT\n$DEV_MENTION\n\n$SONAR_URL\n\n$BRANCH_INFO "  | mail -a difference*.png -s "${CONF_ITEM_GROUP} tag '${TAG}' is just pushed." $@
-    unset SONAR_URL
-}
-
-GET_WEBPAGE_SCREENSHOT ()
-{
-echo "var page = new WebPage(), testindex = 0, loadInProgress = false;
-
-page.onConsoleMessage = function(msg) {
-  console.log(msg);
-};
-
-page.onLoadStarted = function() {
-  loadInProgress = true;
-  console.log(\"load started\");
-};
-
-page.onLoadFinished = function() {
-  loadInProgress = false;
-  console.log(\"load finished\");
-};
-
-var stepName = [\"page open\", \"insert id pw\", \"login\", \"render\"];
-var steps = [
-function() {
-  page.viewportSize = { width: 1980, height: 1200 }
-  page.open(\"http://10.10.32.101/$2/$1/compare/$3...$4?view=parallel\");
-},
-function() {
-  page.evaluate(function() {
-    document.getElementById('user_login').value = 'maxgauge';
-    document.getElementById('user_password').value = 'dev7u8i9o0p';
-  });
-},
-function() {
-  page.evaluate(function() {
-    document.getElementById('new_user').submit();
-  });
-},
-function() {
-   page.render('difference_$3..$4.png');
-  }
-  ];
-
-  interval = setInterval(function() {
-    if (!loadInProgress && typeof steps[testindex] == \"function\") {
-      console.log(stepName[(testindex)] + \" start\");
-      steps[testindex]();
-      testindex++;
-    }
-    if (typeof steps[testindex] != \"function\") {
-      console.log(\"finish!!!!\");
-      phantom.exit();
-    }
-}, 50);" > getpage.js
-
-phantomjs getpage.js
 }
 
 UPDATE_THIS_FILE ()
