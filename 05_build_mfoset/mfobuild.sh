8343fb580d22a569de9f0786f67fa9562b3e72b8
bsh <beaksh90@ex-em.com>
2017-07-20 09:59:33 +0900

    1. .gitlab-ci.yml 내 실행구조 변경
    최초 실행 파일 변경
    * mfoset.sh -> mfobuild.sh 스크립트로 변경
    
    ___
    2. mfobuild.sh 추가
    
    현 형상항목 checkout 명령실행 중 실행중인 스크립트를 변경할 때,
    confilct가 발생한다. mfobuild.sh은 checkout 명령을 수행하는
    처음과 끝부분은 분리하여 기입하고 변경사항이 없도록 할 예정이다.
    
    ___
    3. mfoset.sh
    
    mfoset_dynamic.sh 파트부분 다시 흡수. ( 170125 이전과 같음 )
    
    checkout중 스크립트 파일 confilct가 나는 것을 해결하고자
    mfoset.sh에서 동적인 부분을 mfoset_dynamic.sh 따로 분리하였지만
    (unipjs)형상항목이 추가되어 mfoset.sh 부분이 변경되면서 confilct문제가 발생할 예정.
    
    따라서 변경사항이 영구적으로 없을 부분만 따로 모아 mfobuild.sh로 기입하여
    해결하도록 처리함
    
    추후 그리고 이전버전 빌드시에 잘 작동할 것으로 수계산을 마쳤다.
    
    다만 개별 client에서 요청하는 패치동작에는 mfoset.sh을 먼저 호출하는
    빌드동작을 api로 call하기에 api key를 변경해주는 작업이 필요함
    여러가지 방법중 가장 적은 손실이 작다고 판단.
    
    ___
    4. mfoset_dynamic.sh 형상관리 종료
    
    ___
    5. unipjs에 대한 빌드스크립트 형상관리 시작
    
    ___
    6. unipjs ant 스크립트의 경우 unipjs에 귀속 및 형상관리를 부탁드렸음

---
+++ 05_build_mfoset/mfobuild.sh
@@ -0,0 +1,83 @@
+## Written by EXEM Co., Ltd. DEVQA BSH
+## Last modified 2017.07.19
+##
+## QA가 빌드할 때, 먼저 스크립트(mfoset.sh)가 실행된다. 
+## 스크립트 끝에는 mfobuild 항목을 특정시점으로 Checkout하는데 
+## 현 실행중인 스크립트(mfoset.sh)가 변경사항이 있는 경우 
+## Git에서 변경을 시도 VS OS에서 locking, 즉, Conflict가 발생한다. 
+##
+## 이를 커버하기 위해 checkout 명령을 수행하는 파일은 변경사항이 영원히 없도록 한다.
+
+## Default source Directory
+export BUILD_DIR="C:/Multi-Runner/mfobuild"
+export KEEP_EMPTY_SCRIPT_DIR="C:/Multi-Runner/mfobuild/06_etc"
+export MAIN_DIR="C:/Multi-Runner"
+
+echo "the first step is setting git_tag "
+
+GET_IPADDRESS_GIT_SERVER ()
+{
+	PART="GIT"
+	WHO="QA"
+	## Here are choices of PART. 
+	## REPO, TARGET, GIT
+	## Here are choices of WHO.
+	## DEV, QA
+
+	echo "SET PAGESIZE 0 FEEDBACK OFF VERIFY OFF HEADING OFF ECHO OFF;
+	select ipaddr from ipaddress
+	where PART='$PART'
+	and WHO='$WHO';" > checkout_tag.sql
+	export GIT_IPADDR=`echo exit | sqlplus -silent git/git@DEVQA23 @checkout_tag.sql`
+	sleep 1
+	rm checkout_tag.sql
+	echo "GIT SERVER IPADDRESS = [ $GIT_IPADDR ] "
+}
+
+FETCH_TAG_VER_BUILD ()
+{
+	COMP_TAG="MFOBUILD_TAG"
+	## Here are choices of COMP_TAGs. 
+	## MFOSQL_TAG, MFOWEB_TAG, MFODG_TAG, MFONP_TAG
+	## MFOPG_TAG, MFORTS_TAG, MFOBUILD_TAG 
+		
+	echo "
+	SET PAGESIZE 0 FEEDBACK OFF VERIFY OFF HEADING OFF ECHO OFF;
+	select $COMP_TAG from mfo_tag t join runner_stat r
+	on t.MFO_RELEASE_VER = r.TOTAL_VER
+	where r.RUN_COMP='mfototal_win';" > checkout_tag.sql
+
+	TAG=`echo exit | sqlplus -silent git/git@DEVQA23 @checkout_tag.sql`
+	sleep 1
+	rm checkout_tag.sql
+	cd $BUILD_DIR
+	git checkout $TAG
+}
+
+MFOBUILD_PART()
+{
+cd $BUILD_DIR
+sh $KEEP_EMPTY_SCRIPT_DIR/recoverkeep.sh
+git fetch git@${GIT_IPADDR}:mfo/mfobuild.git --tag
+FETCH_TAG_VER_BUILD
+sh $KEEP_EMPTY_SCRIPT_DIR/removekeep.sh
+}
+
+CHECKOUT_MASTER_MFOBUILD()
+{
+	cd $BUILD_DIR
+	sh $KEEP_EMPTY_SCRIPT_DIR/recoverkeep.sh
+	git checkout master
+}
+
+RECOVER_CONFLICT_FILE()
+{
+	git checkout ${BUILD_DIR}/05_build_mfoset/mfoset_dynamic_part.sh
+	git checkout ${BUILD_DIR}/05_build_mfoset/mfoset.set
+}
+
+GET_IPADDRESS_GIT_SERVER
+MFOBUILD_PART
+sh $BUILD_DIR/05_build_mfoset/mfoset.sh;
+## AS of 170719 , 'mfoset.sh , mfoset_dynamic_part.sh => mfoset.sh'
+CHECKOUT_MASTER_MFOBUILD
--- 05_build_mfoset/mfoset.sh
+++ 05_build_mfoset/mfoset.sh
@@ -5,6 +5,7 @@
 
 ## Default source Directory
 export NPSRC_DIR="C:/Multi-Runner/mfonp"
+export UNIPJS_DIR="C:/Multi-Runner/unipjs"
 export WEBSRC_DIR="C:/Multi-Runner/mfoweb"
 export SQLSRC_DIR="C:/Multi-Runner/mfosql"
 export DGSRC_DIR="C:/Multi-Runner/mfodg"
@@ -50,42 +51,26 @@ GET_IPADDRESS_GIT_SERVER ()
 
 FETCH_TOTAL_VER_INFO ()
 {
-	COMP_TAG="MFO_RELEASE_VER"
-	## Here are choices of COMP_TAGs. 
-	## MFOSQL_TAG, MFOWEB_TAG, MFODG_TAG, MFONP_TAG
-	## MFOPG_TAG, MFORTS_TAG, MFOBUILD_TAG , MFO_RELEASE_VER
-		
-	echo "
-	SET PAGESIZE 0 FEEDBACK OFF VERIFY OFF HEADING OFF ECHO OFF;
-	select $COMP_TAG from mfo_tag t join runner_stat r
-	on t.MFO_RELEASE_VER = r.TOTAL_VER
-	where r.RUN_COMP='mfototal_win';" > checkout_tag.sql
-
-	TAG=`echo exit | sqlplus -silent git/git@DEVQA23 @checkout_tag.sql`
-	sleep 1
-	rm checkout_tag.sql
-	export MFO_PACKAGE_VER=${TAG}
-	echo "MFO RELEASE VERION = [ $TAG ] "
-	
-	
 	## 자잘한 모든 형상항목의 버전을 다 보여준다.
 	echo "
 	SET PAGESIZE 0 FEEDBACK OFF VERIFY OFF HEADING OFF ECHO OFF;
-	select MFONP_TAG,MFOWEB_TAG,MFOSQL_TAG,MFODG_TAG,MFOBUILD_TAG from mfo_tag t join runner_stat r
+	select MFO_RELEASE_VER, MFONP_TAG, MFOWEB_TAG, MFOSQL_TAG, MFODG_TAG, MFOBUILD_TAG from mfo_tag t join runner_stat r
 	on t.MFO_RELEASE_VER = r.TOTAL_VER
 	where r.RUN_COMP='mfototal_win';" > checkout_tag.sql
 
 	TAG=`echo exit | sqlplus -silent git/git@DEVQA23 @checkout_tag.sql`
 	sleep 1
 	rm checkout_tag.sql
-	export MFONP_TAG_VALUE=`echo $TAG | awk '{print $1}'`
-	export MFOWEB_TAG_VALUE=`echo $TAG | awk '{print $2}'`
-	export MFOSQL_TAG_VALUE=`echo $TAG | awk '{print $3}'`
-	export MFODG_TAG_VALUE=`echo $TAG | awk '{print $4}'`
-	export MFOBUILD_TAG_VALUE=`echo $TAG | awk '{print $5}'`
+	export MFO_PACKAGE_VER=`echo $TAG | awk '{print $1}'`
+	export UNIPJS_TAG_VALUE=`echo $TAG | awk '{print $2}'`
+	export MFOWEB_TAG_VALUE=`echo $TAG | awk '{print $3}'`
+	export MFOSQL_TAG_VALUE=`echo $TAG | awk '{print $4}'`
+	export MFODG_TAG_VALUE=`echo $TAG | awk '{print $5}'`
+	export MFOBUILD_TAG_VALUE=`echo $TAG | awk '{print $6}'`
 ##	MFORTS_TAG_VALUE=`echo $TAG | awk '{print $6}'`
-	
-	echo "MFONP_TAG VERION    = [ $MFONP_TAG_VALUE ] "
+
+	echo "MFO RELEASE VERION  = [ $MFO_PACKAGE_VER ] "
+	echo "UNIPJS_TAG VERION   = [ $UNIPJS_TAG_VALUE ] "
 	echo "MFOWEB_TAG VERION   = [ $MFOWEB_TAG_VALUE ] "
 	echo "MFOSQL_TAG VERION   = [ $MFOSQL_TAG_VALUE ] "
 	echo "MFODG_TAG VERION    = [ $MFODG_TAG_VALUE ] "
@@ -96,7 +81,8 @@ FETCH_TOTAL_VER_INFO ()
 TAG_VALUE_VAILD_CHECK ()
 {
 TAG_VAILD_ISSUE=0
-
+## RELEASE VERSION 제외 
+TAG=`echo $TAG | awk '{print $2" "$3" "$4" "$5" "$6}'`
 for TAG_VER in $TAG
 do
 	CONF_ITEM=`echo $TAG_VER | awk -F "_" '{print $1}'`
@@ -132,41 +118,203 @@ else
 fi
 }
 
-FETCH_TAG_VER_BUILD ()
+FETCH_TAG_VER_DG ()
 {
-	COMP_TAG="MFOBUILD_TAG"
-	## Here are choices of COMP_TAGs. 
-	## MFOSQL_TAG, MFOWEB_TAG, MFODG_TAG, MFONP_TAG
-	## MFOPG_TAG, MFORTS_TAG, MFOBUILD_TAG 
-		
+	cd $DGSRC_DIR
+	git checkout $MFODG_TAG_VALUE
+}
+
+FETCH_TAG_VER_NP ()
+{
+	cd $UNIPJS_DIR
+	git checkout $UNIPJS_TAG_VALUE
+}
+
+FETCH_TAG_VER_WEB()
+{
+	cd $WEBSRC_DIR
+	git checkout $MFOWEB_TAG_VALUE
+}
+
+FETCH_TAG_VER_SQL ()
+{
+	cd $SQLSRC_DIR
+	git checkout $MFOSQL_TAG_VALUE
+}
+
+NP_PART()
+{
+cd $SQLSRC_DIR
+git fetch git@${GIT_IPADDR}:mfo/mfosql.git --tag
+FETCH_TAG_VER_SQL
+
+cd $WEBSRC_DIR
+git fetch git@${GIT_IPADDR}:mfo/mfoweb.git --tag
+FETCH_TAG_VER_WEB
+
+cd $UNIPJS_DIR
+git fetch git@${GIT_IPADDR}:uni/unipjs.git --tag
+FETCH_TAG_VER_NP
+
+### VALUE=1 require, 2 Compile&Build, 3 Send File to requirer, 0 Stand by
+echo "
+update runner_stat set value='2' where run_comp='mfototal_win';" > checkout_tag.sql
+echo exit | sqlplus -silent git/git@DEVQA23 @checkout_tag.sql
+sleep 1
+rm checkout_tag.sql
+
+sh $BUILD_DIR/02_build_mfonp/unipjsbuild.sh;
+}
+
+DG_PART()
+{
+cd $MAIN_DIR/mfosms
+git fetch git@${GIT_IPADDR}:mfo/mfosms.git --tag
+
+cd $MAIN_DIR/mfomail
+git fetch git@${GIT_IPADDR}:mfo/mfomail.git --tag
+
+cd $MAIN_DIR/mfoapi
+git fetch git@${GIT_IPADDR}:mfo/mfoapi.git --tag
+
+cd $DGSRC_DIR
+git fetch git@${GIT_IPADDR}:mfo/mfodg.git --tag
+FETCH_TAG_VER_DG
+
+### VALUE=1 require, 2 Compile&Build, 3 Send File to requirer, 0 Stand by
+echo "
+update runner_stat set value='2' where run_comp='mfototal_win';" > checkout_tag.sql
+echo exit | sqlplus -silent git/git@DEVQA23 @checkout_tag.sql
+sleep 1
+rm checkout_tag.sql
+
+sh $BUILD_DIR/01_build_mfodg/dgbuild.sh;
+}
+
+INNOSETUP_AND_PG_PART()
+{
+## 2016.11.11부터 PG를 형상관리하지 않도록 로직을 변경하였다.
+## Postgres DB 엔진설치, DB 설치, 유저생성, DGServer.jar install을 차례대로 실행하고 
+## 그렇게 만들어진 폴더를 패키징하는 것으로 로직을 변경하였다. 
+
+cd $PG_INSTALL_FILE
+sh $PG_INSTALL_FILE/ready_pg.sh
+## FOR DEMO IN ORDER TO REDUCE PROCESS TIME UNTIL 2017.05.22
+sh $BUILD_DIR/05_build_mfoset/Innosetup.sh;
+$PG_INSTALL_FILE/detach_pg.bat
+}
+
+RENAME_NP_FOR_DEPLOY ()
+{
+	if [ -d "$PACKAGE_DIR/$MFO_PACKAGE_VER" ]; then
+		echo " There is duplicate Dir"
+		echo " Existing Dir is going to be removed"
+		rm -r $PACKAGE_DIR/$MFO_PACKAGE_VER
+	fi
+
+	## DIRECTORY '$PACKAGE_DIR/$MFO_PACKAGE_VER' WILL COLLECT EVERY FILES ABOUT DEPLOY.
+	## 아래 생성되는 디렉토리에 이번에 만든 패키지 및 각종 설치파일을 모아둠
+	
+	mkdir $PACKAGE_DIR/$MFO_PACKAGE_VER
+	cd $UNIPJS_DIR/deploy/uni/zip
+	PJS_FILE=`ls PlatformJS*.zip`
+	PJS_BUILD_NUMBER=`cat $WEBSRC_DIR/common/VersionControl.js | grep "var BuildNumber" | awk -F "'" '{print $2}' | awk -F "." '{print $1"."$2"."$3}'`
+	PJS_DATE=`cat $WEBSRC_DIR/common/VersionControl.js | grep "var BuildNumber" | awk -F "'" '{print $2}' | awk -F "." '{print $4}'`
+	cp -v $PJS_FILE $PACKAGE_DIR/$MFO_PACKAGE_VER/[MFO${PJS_BUILD_NUMBER}]_[PlatformJS]_[$PJS_DATE].zip
+}
+
+RENAME_DG_FOR_DEPLOY ()
+{
+	cd $DGSRC_DIR/deploy/MFO/tar
+	DG_TAR_FILE=`ls Maxgauge*.tar`
+	DGS_BUILD_NUMBER=`echo $DG_TAR_FILE | awk -F "MaxGauge" '{print $2}' | awk -F "_" '{print $1}'`
+	DGS_DATE=`echo $DG_TAR_FILE | awk -F "." '{print $3}' | awk -F "_" '{print $2}'`
+	cp -v $DG_TAR_FILE $PACKAGE_DIR/$MFO_PACKAGE_VER/[MFO${DGS_BUILD_NUMBER}]_[DataGather]_[$DGS_DATE].tar
+}
+
+RENAME_INNOSETUPFILES_FOR_DEPLOY ()
+{
+cd $PACKAGE_DIR
+
+case $REQ_TAG in
+	total)
+	## totalwopjs 는 INNOSETUP패키지 파일 2개중 PJS만 있는 것을 제외하고 만든다.
+	## FOR DEMO IN ORDER TO REDUCE PROCESS TIME UNTIL 2017.05.22
+	## mv -> cp  UNTIL 2017.05.22
+		PJS_ONLY=`ls *ONLY_PJS*`
+		mv $PJS_ONLY  $PACKAGE_DIR/$MFO_PACKAGE_VER/[MFO${PJS_BUILD_NUMBER}]_[PlatformJS]_[$PJS_DATE].exe
+		TOTAL_PACKAGE=`ls *MaxGauge* | grep -v "ONLY_PJS"`
+		mv $TOTAL_PACKAGE $PACKAGE_DIR/$MFO_PACKAGE_VER/[MFO${PJS_BUILD_NUMBER}]_[Full_Setup]_[$PJS_DATE].exe
+	;;
+	totalwopjs)
+		TOTAL_PACKAGE=`ls *MaxGauge*`
+		mv $TOTAL_PACKAGE $PACKAGE_DIR/$MFO_PACKAGE_VER/[MFO${PJS_BUILD_NUMBER}]_[Full_Setup]_[$PJS_DATE].exe
+	;;
+esac
+}
+
+WRITE_DOWN_TAG_INFO ()
+{
 	echo "
 	SET PAGESIZE 0 FEEDBACK OFF VERIFY OFF HEADING OFF ECHO OFF;
-	select $COMP_TAG from mfo_tag t join runner_stat r
+	select  mfo_release_ver, mfonp_tag, mfoweb_tag, mfosql_tag, mfodg_tag, mforts_tag from mfo_tag t join runner_stat r
 	on t.MFO_RELEASE_VER = r.TOTAL_VER
 	where r.RUN_COMP='mfototal_win';" > checkout_tag.sql
 
 	TAG=`echo exit | sqlplus -silent git/git@DEVQA23 @checkout_tag.sql`
 	sleep 1
 	rm checkout_tag.sql
-	cd $BUILD_DIR
-	git checkout $TAG
+	cd $PACKAGE_DIR/$MFO_PACKAGE_VER
+	for i in $TAG
+	do
+	echo -e "$i\n" >> TAG_INFO.txt;
+	done
 }
 
-MFOBUILD_PART()
+SEND_DG_PJS_LINUX_BUILD_SRV ()
 {
-cd $BUILD_DIR
-sh $KEEP_EMPTY_SCRIPT_DIR/recoverkeep.sh
-git fetch git@${GIT_IPADDR}:mfo/mfobuild.git --tag
-FETCH_TAG_VER_BUILD
-sh $KEEP_EMPTY_SCRIPT_DIR/removekeep.sh
+sh ${KEEP_EMPTY_SCRIPT_DIR}/sendfile_linux_packaging.sh;
+}
+
+BUILD_AS_REQ_ORDER()
+{
+case $REQ_TAG in
+	totalwopjs|total)
+## PlatformJS & DataGather ( total - CI PROCESS )
+## total은 INNOSETUP패키지&리눅스 자동설치까지 포함하는 개념이다.
+## totalwopjs 는 INNOSETUP패키지 파일 2개중 PJS만 있는 것을 제외하고 만든다.
+	DG_PART
+	NP_PART
+	INNOSETUP_AND_PG_PART
+	## PACKAGE DIR MADE BY FUNC 'RENAME_NP_FOR_DEPLOY'
+	RENAME_NP_FOR_DEPLOY
+	RENAME_DG_FOR_DEPLOY
+	RENAME_INNOSETUPFILES_FOR_DEPLOY
+	WRITE_DOWN_TAG_INFO
+	SEND_DG_PJS_LINUX_BUILD_SRV
+	;;
+	nwsd|nwd|nsd|nd|wsd|wd|sd)
+## PlatformJS & DataGahter
+	DG_PART
+	NP_PART
+	;;
+	nws|nw|ns|n|ws|w|s)
+## Only PlatformJS
+	NP_PART
+	;;
+	d)
+## Only DataGather
+	DG_PART
+	;;
+esac
 }
 
 REQUIRER_CHECK
 GET_IPADDRESS_GIT_SERVER
 FETCH_TOTAL_VER_INFO
 TAG_VALUE_VAILD_CHECK
-MFOBUILD_PART
 sh $BUILD_DIR/05_build_mfoset/mfoset_dynamic_part.sh;
 ## AS of 170125 Dynamic Part was seperated in the 'mfoset_dynamic_part'
-# BUILD_AS_REQ_ORDER
+## AS of 170719 Revert changes of 170125.
+BUILD_AS_REQ_ORDER
 # CHECKOUT_MASTER_MFOBUILD
